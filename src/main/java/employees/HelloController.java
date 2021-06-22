b71885627963f6180f0cdbf3dfad3d25c2ce239e
n0rb1v <n0rb1v@gmail.com>
2021-06-22 11:43:53 +0200

    init

---
+++ src/main/java/employees/HelloController.java
@@ -0,0 +1,16 @@
+package employees;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello Spring Boot " + LocalDateTime.now();
+    }
+}
